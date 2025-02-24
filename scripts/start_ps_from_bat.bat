950da2bd601935e1b68a7f7a1b4f4244d0de65b1
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-02-24 18:56:29 +0100

    exec policy

--- scripts/start_ps_from_bat.bat
+++ scripts/start_ps_from_bat.bat
@@ -2,7 +2,7 @@
 echo Starting Shell %~f0
 set HERE=%CD%
 echo BATCH in %HERE%
-powershell -executionpolicy remotesigned -File  %HERE%\setup_python.ps1 %HERE%
+powershell -executionpolicy bypass -File  %HERE%\setup_python.ps1 %HERE%
 
 pause
 :: %comspec% /K title Python %PYTHON_HOME%
